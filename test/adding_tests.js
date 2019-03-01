54b07c1c03a4d55accc8a1baa3118e702b4d4045
Sam Phelan <sep445@nyu.edu>
2019-02-28 23:08:59 -0500

    creating testing director

---
+++ test/adding_tests.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const expect = chai.expect; 
+
+describe('Oink Adding Tests', function(){
+	// CHECK COLLISION FUNCTION TESTA
+	describe('addFriend', function(){
+		// DOES NOT ALLOW HEAD OF SNAKE TO EQUAL ANY PART OF THE SNAKE'S BODY
+		it('returns Friend object of newly added Friend', function(){
+			const friend = {};
+			friend.username = "testName";
+			friend.email = "test@email.com";
+			const res = app.addFriend(friend.username);
+			expect(res).to.equal(friend);
+		});
+	});
+
+	describe('addGroup', function(){
+		// DOES NOT ALLOW SNAKE TO EXCEED BOUNDS OF CANVAS FRAME
+		it('returns true if group created successfully', function(){
+			const friend1 = {username: "testName", email: "test@email.com"};
+			const friend2 = {username: "testName2", email: "test2@email.com"};
+			const friends = [friend1, friend2];
+			const name = "friends";
+			const res = app.addGroup(name, friends);
+			expect(res).to.be.true;
+		});	
+	});
+});
