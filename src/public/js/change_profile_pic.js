744b4cc5ba226b10649ec1095b68e75b1036821f
jess-ingraham <jli232@nyu.edu>
2019-04-22 12:51:27 -0400

    adding back functionality for uploading picture

---
+++ src/public/js/change_profile_pic.js
@@ -0,0 +1,10 @@
+function openForm(){
+
+	document.querySelector("form#change-profile-pic").classList.remove("hidden");
+	//document.body.scroll = none;
+
+}
+
+function closeForm(){
+	document.querySelector("form#change-profile-pic").classList.add("hidden");
+}
