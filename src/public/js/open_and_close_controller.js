eed7dfbbbb682068fef2c4aac3731422676fcf08
jess-ingraham <jli232@nyu.edu>
2019-04-22 15:24:41 -0400

    adding open and close with state pattern

--- src/public/js/open_and_close_controller.js
+++ src/public/js/open_and_close_controller.js
@@ -1,18 +1,45 @@
         //if the Nav is open, close it, and vice versa
 
+class ControlSideNav {
+
+	constructor(starting_state){
+		this.state = starting_state;
+
+	}
+
+	get_state(){
+		return this.state;
+	}
+
+	change_nav(){
+		console.log(this.state);
+		this.state.change_nav(this);
+	}
+
+	set_state(state){
+		this.state = state;
+	}
+
+	perform_behavior(){
+		this.state.perform_behavior();
+	}
+
+}
+
 class OpenSideNav {
 	constructor(){}
 
 	perform_behavior(){
+
 		document.querySelector("nav.main-side-nav").style.width = "16%";
         document.querySelector("div.container").style.marginLeft = "16%";
         document.querySelector("div.left-side-header i.fa-bars").classList.add("hidden");
         document.querySelector("div.left-side-header i.fa-times").classList.remove("hidden")
 	}
 
-   changeNav(ControlSideNav sideNav){
-      sideNav.set_state(new CloseSideNav());
-  }
+	change_nav(sideNav){
+		sideNav.set_state(new CloseSideNav());
+   }
 
 }
 
@@ -27,38 +54,25 @@ class CloseSideNav {
 		document.querySelector("div.left-side-header i.fa-times").classList.add("hidden");
 	}
 	
-	changeNav(ControlSideNav sideNav){
-       sideNav.set_state(new OpenSideNav);
+	change_nav(sideNav){
+		sideNav.set_state(new OpenSideNav());
    	}
 }
 
-class ControlSideNav {
 
-	constructor(starting_state){
-		this.state = starting_state;
-
-	}
-
-	change_nav(){
-		this.state.change_nav(this);
-	}
-
-	set_state(state){
-		this.state = state;
-	}
-
-	perform_behavior(){
-		this.state.perform_behavior();
-	}
-
-}
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
-	sideNavControl = new ControlSideNav(new CloseSideNav);
+	sideNavControl = new ControlSideNav(new CloseSideNav());
+	console.log("content loaded");
 
-	document.querySelector("div.left-side-header").addEventListener("click", ()=>{
+	document.querySelector("div.left-side-header i.fa-bars").addEventListener("click", ()=>{
+		sideNavControl.change_nav();
+		sideNavControl.perform_behavior();
+	});
+
+	document.querySelector("div.left-side-header i.fa-times").addEventListener("click", ()=>{
 		sideNavControl.change_nav();
 		sideNavControl.perform_behavior();
 	});
