193ae1b2393d81352cf531fe654b4de52adf504b
Kshoes <kevinwxu@gmail.com>
2019-04-25 13:55:26 -0400

    starting unit tests for addGroup.js

---
+++ test/addGroup_tests.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const expect = chai.expect; 
+const api = require("../src/public/js/addGroup.js");
+
+const mongoose = require('mongoose');
+require('../src/schemas');
+const User = mongoose.model("User");
+const Group = mongoose.model("Group");
+
+describe('Add Group Tests', function(){
+
+    describe('onClickAddUserToGroup', function(){
+
+        it('returns true if user search bar is cleared'), function(){
+            const input = document.getElementById("searchUser").value;
+            
+        }
+
+        it('returns true ')
+
+
+    })
+
+
+})
+
