0fb6164ccd9cc910fe95ea12c39cee1103348ee9
Dorna Mu <dornamu@10-16-194-213.dynapool.wireless.nyu.edu>
2019-04-25 12:52:05 -0400

    fixing code we are servivng the landing page accurately

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router(); 
-
+const path = require('path')
 router.get('/', (req, res) => {
 	if(req.session.user){
 		res.redirect(`/user/${req.session.user.username}`);
 	}
 	else{
-  		res.render('index');
+  		res.sendFile(path.join(__dirname, "..", "public", "html", 'index.html'))
 	}
 
 });
