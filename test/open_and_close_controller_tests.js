59c65a614d7eaa7406ccffe1f68b821bfaac9cb3
Kshoes <kevinwxu@gmail.com>
2019-05-02 13:14:50 -0400

    creating test file for open_and_close_controller.js

---
+++ test/open_and_close_controller_tests.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect; 
+const api = require("../src/public/js/open_and_close_controller.js");
+
+const mongoose = require('mongoose');
+require('../src/schemas');
+const User = mongoose.model("User");
+const Group = mongoose.model("Group");
+
+describe('Open/Close controller tests', function(){
+
+
+
+
+})
